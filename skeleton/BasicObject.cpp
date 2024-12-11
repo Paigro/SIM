fe3112deacb265e0604ff2a39a30e2a5c5f0074b
Paigro <paigro9@gmail.com>
2024-12-11 12:31:25 +0100

    Antes de crear otra rama.

---
+++ skeleton/BasicObject.cpp
@@ -0,0 +1,4 @@
+#include "BasicObject.h"
+
+
+
---
+++ skeleton/BasicObject.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <PxPhysicsAPI.h>
+#include "core.hpp"
+#include "RenderUtils.hpp"
+
+using namespace physx;
+
+
+class BasicObject
+{
+private:
+
+
+
+protected:
+
+
+
+public:
+
+	//------
+
+	//
+	BasicObject();
+	//
+	BasicObject();
+
+
+	//------
+
+	//
+	virtual bool update(float t);
+
+
+};
